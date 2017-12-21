fe3e478654e1cca33cb4c59b21b968dd4f62e7df
Arkhen <Arkhen@DESKTOP-NRG4CR4>
2017-12-21 18:02:06 +0000

    Cambio fecha

--- src/kata1/Kata1.java
+++ src/kata1/Kata1.java
@@ -1,11 +1,11 @@
 package kata1;
-
+ 
 import java.util.Date;
 
 public class Kata1 {
 
     public static void main(String[] args) {
-        Person person = new Person("David", new Date(49,8,24));
+        Person person = new Person("David", new Date(89,9,11));
         System.out.println(person.getName() + " tiene " + person.getAge());
     }
     
